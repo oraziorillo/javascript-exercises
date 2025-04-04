9eb1421b782c2d9a821fbb8aea350fbffc5e8e5a
oraziorillo <oraziorillo@gmail.com>
2025-04-04 17:42:14 +0200

    solved 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    if (n < 0) return 'ERROR';
+    let result = '';
+    for (let i = 0; i < n; i++) {
+        result += s;
+    }
+    return result;
 };
 
 // Do not edit below this line
