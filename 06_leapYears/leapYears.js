cb10cca249c2bdb8d2e9c341eceb7119fe61e7ac
oraziorillo <oraziorillo@gmail.com>
2025-04-04 18:16:07 +0200

    solved 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year) || year < 0) {
+        return 'ERROR';
+    }
 
+    if (!Number.isInteger(year / 4)) {
+        return false;
+    }
+    if (Number.isInteger(year / 10) && !Number.isInteger(year / 40)) {
+        return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
