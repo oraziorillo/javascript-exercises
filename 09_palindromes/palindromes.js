900d225b862ce2e079d07714917e12cd7869763d
oraziorillo <oraziorillo@gmail.com>
2025-04-09 11:34:35 +0200

    solved 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    // Remove all non-alphanumeric characters and convert to lowercase
+    s = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== s[s.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
