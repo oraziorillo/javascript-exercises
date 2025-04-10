7d0a204308baf3e17ac6473b3da10888216937c3
oraziorillo <oraziorillo@gmail.com>
2025-04-10 11:52:16 +0200

    solved 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    // Check if n is a string
+    if (typeof n === 'string') {
+        // Convert string to number
+        n = parseInt(n);
+    }
+    if (n < 0) {
+        return "OOPS";
+    }
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    let first = 0;
+    let second = 1;
+    for (let i = 2; i < n + 1; i++) {
+        let curr = first + second;
+        first = second;
+        second = curr;
+    }
+    return second;
 };
 
 // Do not edit below this line
